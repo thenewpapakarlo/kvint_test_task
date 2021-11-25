464e3d98efdfdf0d77f1c5a2551f8f658c270397
Alexander Pobezhimov <thenewpapakarlo@gmail.com>
2021-11-25 18:30:03 +0300

    All done!

--- tests/test_state_model.py
+++ tests/test_state_model.py
@@ -3,9 +3,18 @@ import pytest
 
 
 model = PizzaOrderModel()
+messages = [
+    'Какую вы хотите пиццу? Большую или маленькую?',
+    'Как вы будете платить?',
+    'Вы хотите {} пиццу, оплата - {}?',
+    'Спасибо за заказ'
+]
+i = 0
 
 
 @pytest.mark.parametrize('trigger', ['/start', 'Большую', 'Наличкой', 'Да'])
 def test_change_state(trigger):
     model.change_state(trigger)
-    model.state
+    global i
+    assert messages[i] == model.questions[model.state]
+    i += 1
